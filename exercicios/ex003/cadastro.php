48d3d3126be195741f991f4ede5f9d71be90ae66
tomas <tomas153lm@gmail.com>
2023-06-30 16:03:13 -0300

    finish: exercicios

--- exercicios/ex003/cadastro.php
+++ exercicios/ex003/cadastro.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Cadastro</title>
 </head>
 <body>
-    <h1>Exemplo de PHP</h1>
-
     <?php 
-        date_default_timezone_set("America/Sao_Paulo");
-        echo "<br>A hora atual é: " . date("G:i:s");
+        echo "O seu nome é: " . $_GET["nome"];
+        echo "A senha digitada foi: " . $_GET["senha"];
     ?>
 </body>
 </html>
